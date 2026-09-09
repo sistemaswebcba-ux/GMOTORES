711d7399fb1847745f6b6bcd3e523f6c60797c9e
Pablo <44281098+pablomartinzabala2@users.noreply.github.com>
2026-09-10 00:16:47 +0200

    9-9
    9-9

--- Concesionaria/Concesionaria/Program.cs
+++ Concesionaria/Concesionaria/Program.cs
@@ -15,8 +15,9 @@ namespace Concesionaria
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-              Application.Run(new FrmLogin());
-           // Application.Run(new FrmSql());
+            //  Application.Run(new FrmLogin());
+
+               Application.Run(new FrmBoletoGMotor());
             //    Application.Run(new FrmListadoAvisos ());
             //    //   Application.Run(new FrmConsultaAgenda());
             // A//pplication.Run(new FrmAbmEntidad());
